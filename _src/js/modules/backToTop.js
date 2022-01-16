34fa177714beea6a21db42c5e0938d173891fe29
unknown <al.tulin@gmail.com>
2022-01-16 20:40:43 +0800

    add back to top

--- _src/js/modules/backToTop.js
+++ _src/js/modules/backToTop.js
@@ -1,40 +1,40 @@
-import helpers from '../helpers';
+import helpers from "../helpers";
 
 /**
-* Модуль "Возврат наверх"
-*/
+ * Модуль "Возврат наверх"
+ */
 const init = () => {
-	const className = '.js-back-to-top';
-	const shownClass = 'is-shown';
-	let lastScrollTop = 0;
+  const className = ".js-back-to-top";
+  const shownClass = "is-shown";
+  let lastScrollTop = 0;
 
-	helpers.$document.on('click.backTop', `${className}`, () => {
-		helpers.scrollTo($('body'));
-	});
+  helpers.$document.on("click.backTop", `${className}`, () => {
+    helpers.scrollTo($("body"));
+  });
 
-	helpers.$window.on('scroll.backTop', () => {
-		const scrollTop = window.pageYOffset;
+  helpers.$window.on("scroll.backTop", () => {
+    const scrollTop = window.pageYOffset;
 
-		if (scrollTop > window.innerHeight) {
-			if (lastScrollTop > scrollTop) {
-				$(className).addClass(shownClass);
-			} else {
-				$(className).removeClass(shownClass);
-			}
-		} else {
-			$(className).removeClass(shownClass);
-		}
+    if (scrollTop > window.innerHeight) {
+      if (lastScrollTop > scrollTop) {
+        $(className).addClass(shownClass);
+      } else {
+        $(className).removeClass(shownClass);
+      }
+    } else {
+      $(className).removeClass(shownClass);
+    }
 
-		lastScrollTop = scrollTop;
-	});
+    lastScrollTop = scrollTop;
+  });
 };
 
 const destroy = () => {
-	helpers.$window.off('.backTop');
-	helpers.$document.off('.backTop');
+  helpers.$window.off(".backTop");
+  helpers.$document.off(".backTop");
 };
 
 export default {
-	init,
-	destroy,
+  init,
+  destroy,
 };
